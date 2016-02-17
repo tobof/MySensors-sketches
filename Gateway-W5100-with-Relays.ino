4366bdb3ce22c7d27bd9d2d923df52c1a314f10c
tobof <oberfoell@web.de>
2016-02-17 16:12:22 +0100

    Gateway-W5100-with-Relays

---
+++ Gateway-W5100-with-Relays.ino
@@ -0,0 +1,124 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik EKblad
+ * Contribution by a-lurker and Anticimex,
+ * Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
+ * Contribution by Tomas Hozza <thozza@gmail.com>
+ *
+ *
+ * DESCRIPTION
+ * The EthernetGateway sends data received from sensors to the ethernet link.
+ * The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
+ *
+ * The GW code is designed for Arduino 328p / 16MHz.  ATmega168 does not have enough memory to run this program.
+ *
+ * LED purposes:
+ * - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
+ * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+ * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
+ * - ERR (red) - fast blink on error during transmission error or recieve crc error
+ *
+ * See http://www.mysensors.org/build/ethernet_gateway for wiring instructions.
+ *
+ */
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG 
+
+// Enable gateway ethernet module type 
+#define MY_GATEWAY_W5100
+
+// W5100 Ethernet module SPI enable (optional if using a shield/module that manages SPI_EN signal)
+//#define MY_W5100_SPI_EN 4  
+
+// Enable Soft SPI for NRF radio (note different radio wiring is required)
+// The W5100 ethernet module seems to have a hard time co-operate with 
+// radio on the same spi bus.
+#if !defined(MY_W5100_SPI_EN) && !defined(ARDUINO_ARCH_SAMD)
+  #define MY_SOFTSPI
+  #define MY_SOFT_SPI_SCK_PIN 14
+  #define MY_SOFT_SPI_MISO_PIN 16
+  #define MY_SOFT_SPI_MOSI_PIN 15
+#endif  
+
+#define MY_IP_ADDRESS 192,168,178,66   // If this is disabled, DHCP is used to retrieve address
+
+
+// The port to keep open on node server mode / or port to contact in client mode
+#define MY_PORT 5003      
+
+// The MAC address can be anything you want but should be unique on your network.
+// Newer boards have a MAC address printed on the underside of the PCB, which you can (optionally) use.
+// Note that most of the Ardunio examples use  "DEAD BEEF FEED" for the MAC address.
+#define MY_MAC_ADDRESS 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <MySensor.h>
+
+#define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define NUMBER_OF_RELAYS 1 // Total number of attached relays
+#define RELAY_ON 1  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+
+
+void before() { 
+  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
+    // Then set relay pins in output mode
+    pinMode(pin, OUTPUT);   
+    // Set relay to last known state (using eeprom storage) 
+    digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
+  }
+}
+void setup()
+{
+}
+
+void presentation()  
+{   
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Relay", "1.0");
+
+  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
+    // Register all sensors to gw (they will be created as child devices)
+    present(sensor, S_LIGHT);
+  }
+}
+
+void receive(const MyMessage &message) {
+  // We only expect one type of message from controller. But we better check anyway.
+  if (message.type==V_LIGHT) {
+     // Change relay state
+     digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+     // Store state in eeprom
+     saveState(message.sensor, message.getBool());
+     // Write some debug info
+     Serial.print("Incoming change for sensor:");
+     Serial.print(message.sensor);
+     Serial.print(", New status: ");
+     Serial.println(message.getBool());
+   } 
+}
+
+void loop() {
+}
